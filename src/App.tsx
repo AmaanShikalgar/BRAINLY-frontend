368424d7be335c76acf0604dd07625fc2087dc29
AmaanShikalgar <amaanshikalgar2003@gmail.com>
2026-06-24 22:32:32 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Button } from "./components/ui/Button"
+import './App.css'
 
 function App() {
 
   return(
     <>
-      <Button startIcon={"s"} variant="primary" size="md" onClick={()=>{}} text={"asd"}/>
+      <Button variant="secondary" size="sm" onClick={()=>{}} text="Share"/>
+      <Button variant="primary" size="md" onClick={()=>{}} text="Add Content"/>
+      <Button variant="primary" size="lg" onClick={()=>{}} text="Add Content"/>
     </>
   )
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,14 +1,27 @@
+import type { ReactElement } from "react";
+
 interface ButtonProps {
     variant: "primary"|"secondary";
     size: "sm"|"md"|"lg";
     text: string;
-    startIcon?:any; 
-    endIcon?:any;
+    startIcon?:ReactElement; 
+    endIcon?:ReactElement;
     onClick: ()=> void;
 }
 
-export const Button = (props:ButtonProps) =>{
-    return <button></button>
+const variantStyles = {
+    "primary": "bg-primary text-white",
+    "secondary":"bg-secondary text-primary"
 }
 
-<Button startIcon={"s"} variant="primary" size="md" onClick={()=>{}} text={"asd"}/>
+const sizeStyles = {
+    "sm": "p-2",
+    "md": "p-4",
+    "lg": "p-8"
+}
+
+const defaultStyles = "rounded-md p-4"
+
+export const Button = (props:ButtonProps) => { 
+    return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}>{props.text}</button>
+}
