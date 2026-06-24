4c44fe17ea91a1a67c2d4deff18cd97658440f26
AmaanShikalgar <amaanshikalgar2003@gmail.com>
2026-06-24 17:20:08 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/Button"
+
+function App() {
+
+  return(
+    <>
+      <Button startIcon={"s"} variant="primary" size="md" onClick={()=>{}} text={"asd"}/>
+    </>
+  )
+}
+
+export default App
